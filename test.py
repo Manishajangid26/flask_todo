4ae307053cef460e18e4957619c3e8280fec013f
manisha jangid <manisahjangid12665@gmail.com>
2025-02-28 17:00:25 +0530

    add my flask files

---
+++ test.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/greet")
+def Good():
+    return "Good Morning Sir"
+
+# @app.route("/profile/manisha")
+# def manisha():
+#     return "Hello,Manisha"    
+
+
+# @app.route("/profile/vipin")
+# def vipin():
+#     return "Hello,vipin"            
+
+
+
+@app.route("/profile/<user>")
+def user_name(user):
+    return f"Hello,{user}"
